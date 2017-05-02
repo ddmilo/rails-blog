bce3f98bb11db9017a78fa135c1483effbf1176e
ddmilo <ddmilo1029@gmail.com>
2017-05-02 16:00:06 -0400

    forgot some setup stuff, added devise views

--- rails_blog/config/routes.rb
+++ rails_blog/config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
        sessions: 'users/sessions'
      }
-     
+
   get 'posts/index'
 
   get 'posts/show'
 
   get 'posts/create'
 
-  devise_for :users
+
+  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
