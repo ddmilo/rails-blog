3fb36d8b3440d0b96f765f12e730422257cc5032
ddmilo <ddmilo1029@gmail.com>
2017-05-30 08:10:40 -0400

    working pagination on posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-
+    @posts = @posts.reorder("updated_at DESC").paginate(:page => params[:page], :per_page => 5)
 
 
   end
